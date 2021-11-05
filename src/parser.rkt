e9e2f8eccb9bfe9d15abbd6d26e9e99eeeca7522
Alaia Solko-Breslin <alaiasolko-breslin@dhcp-vl2042-8049.redrover.cornell.edu>
2021-11-05 16:46:24 -0400

    parser

--- src/parser.rkt
+++ src/parser.rkt
@@ -1,52 +1,39 @@
 #lang racket
 
-(require parser-tools/lex
-         (prefix-in re- parser-tools/lex-sre)
-         parser-tools/yacc)
-(provide (all-defined-out))
+(require br-parser-tools/lex)
+(require brag/support)
+(require "bnf.rkt")
 
-; adapted from https://gist.github.com/danking/1068185/b93e3bfc77ccfe58a35179c8502d834c0016adcf
+(define (tokenize ip)
+    (port-count-lines! ip)
+    (define my-lexer
+      (lexer-src-pos
+       [(repetition 1 +inf.0 numeric)
+        (token 'NUM (string->number lexeme))]
+       ["lambda"
+        (token 'LAMBDA "lambda")]
+       ["+"
+        (token 'PLUS "+")]
+       ["-"
+        (token 'MINUS "-")]
+       ["*"
+        (token 'TIMES "*")]
+       ["!"
+        (token 'NOT "!")]                  
+       ["("
+        (token 'LPAREN "(")]
+       [")"
+        (token 'RPAREN ")")]  
+       [whitespace
+        (token 'WHITESPACE lexeme #:skip? #t)]
+      ;  [any-string
+      ;   (token 'ID lexeme)]
+       [(eof)
+        (void)]))
+    (define (next-token) (my-lexer ip))
+    next-token)
 
-(define-tokens a (NUM))
-(define-empty-tokens b (+ - * EOF LPAREN RPAREN))
-(define-lex-trans number
-  (syntax-rules ()
-    ((_ digit)
-     (re-: (re-? (re-or "-" "-")) (uinteger digit)
-           (re-? (re-: "." (re-? (uinteger digit))))))))
-(define-lex-trans uinteger
-  (syntax-rules ()
-    ((_ digit) (re-+ digit))))
-(define-lex-abbrevs
-  (digit10 (char-range "0" "9"))
-  (number10 (number digit10)))
-
-(define lisp-lexer
-           (lexer
-            ((re-+ number10) (token-NUM (string->number lexeme)))
-            ("-" (token--))
-            ("+" (token-+))
-            ("*" (token-*))
-            ("(" (token-LPAREN))
-            (")" (token-RPAREN))
-            ;; recursively calls the lexer which effectively skips whitespace
-            (whitespace (lisp-lexer input-port))
-            ((eof) (token-EOF))))
-
-(define lisp-parser
-           (parser
-            (start exp)
-            (end EOF)
-            (error void)
-            (tokens a b)
-            (precs (left - +))
-            (grammar
-             (exp ((NUM) $1)
-                  ((LPAREN + exp exp RPAREN) (+ $3 $4))
-                  ((LPAREN - exp exp RPAREN) (- $3 $4))
-                  ((LPAREN * exp exp RPAREN) (* $3 $4))))))
-
-(define (lex-this lexer input) (lambda () (lexer input)))
-
-(let ((input (open-input-string "(+ 3 (- 2 1))")))
-           (lisp-parser (lex-this lisp-lexer input)))
+(define a-sample-input-port (open-input-string "(+ 3 2)"))
+(define token-thunk (tokenize a-sample-input-port))
+(define another-stx (parse token-thunk))
+(syntax->datum another-stx)
