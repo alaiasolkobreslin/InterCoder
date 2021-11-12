34c0bcda6a82081466ce9e2d50a443bffc0aa0ba
Alaia Solko-Breslin <alaiasolko-breslin@dhcp-vl2042-8049.redrover.cornell.edu>
2021-11-12 12:47:51 -0500

    plus progress

--- src/synthesize.rkt
+++ src/synthesize.rkt
@@ -5,9 +5,9 @@
 
 (define int32? (bitvector 32))
 
-(define-grammar (plus x y)        ; Grammar of int32 expressions over two inputs:
+(define-grammar (plus x y)        ; Grammar of int expressions over two inputs:
   [expr
-   (choose x y (?? int32?)        ; <expr> := x | y | <32-bit integer constant> |
+   (choose x y (?? integer?)      ; <expr> := x | y | <integer constant> |
            ((bop) (expr) (expr))  ;           (<bop> <expr> <expr>) |
            ((uop) (expr)))]       ;           (<uop> <expr>)
   [bop
@@ -20,28 +20,20 @@
 
 (define (check-plus impl x y)
   (if (and (equal? x 1) (equal? y 2)) 
-    (assert (equal? (impl x y) 3)) 
-    (assert (equal? 1 1))))
-
-
-
-; (define (int32 i)
-;   (bv i int32?))
-
-(check-plus + 1 2)
+    (assert (equal? (impl x y) 3))
+    ; (if (and (equal? x 4) (equal? y 1))
+    ;   (assert (equal? (impl x y) 5))
+      (assert (equal? 1 1))))
 
 (define-symbolic l h integer?)
 
-; (define sol
-;     (synthesize
-;      #:forall    (list l h)
-;      #:guarantee (check-plus plus-interp l h)))
-
-; sol
-
-; (define cex (verify (check-plus + l h)))
-; cex
-
-
-(define cex (verify (check-plus + 1 2)))
+(define cex (verify (check-plus + l h)))
 cex
+
+(define sol
+    (synthesize
+     #:forall    (list l h)
+     #:guarantee (check-plus plus-interp l h)))
+
+sol
+(print-forms sol)
