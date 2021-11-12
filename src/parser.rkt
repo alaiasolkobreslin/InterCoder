c59639f643ec13e5fec2c27fce2c7f9e194c3ba9
Noah Thompson <noah.scott.thompson@gmail.com>
2021-11-12 14:49:44 -0500

    made parser worse

--- src/parser.rkt
+++ src/parser.rkt
@@ -4,6 +4,10 @@
 (require brag/support)
 (require "bnf.rkt")
 
+;;; (struct 
+(struct expr (a b c d e))
+
+
 (define (tokenize ip)
     (port-count-lines! ip)
     (define my-lexer
@@ -33,7 +37,42 @@
     (define (next-token) (my-lexer ip))
     next-token)
 
-(define a-sample-input-port (open-input-string "(+ 3 2)"))
-(define token-thunk (tokenize a-sample-input-port))
-(define another-stx (parse token-thunk))
-(syntax->datum another-stx)
+;;; (define (simplify1 stx)
+;;;     (match stx
+;;;         ;;; [(ID) ID]
+;;;         ;;; [(expr "(" e ")") e]
+;;;         ;;; [(expr _) 3]
+;;;         [(expr a x y z d) 2]
+;;;         ;;; [(e) (e)]
+;;;         ;;; [(LPAREN PLUS expr expr RPAREN) (PLUS expr expr)]
+;;;         ;;; [(LPAREN MINUS expr expr RPAREN) (MINUS expr expr)]
+;;;         ;;; [(LPAREN TIMES expr expr RPAREN) (TIMES expr expr)]
+;;;         ;;; [(LPAREN NOT expr RPAREN) (NOT expr)]
+;;;         ;;; [(LPAREN MINUS expr RPAREN) (MINUS expr)]
+;;;         ;;; [(LPAREN expr expr RPAREN) (APP expr expr)]
+;;;         ;;; [(LPAREN LAMBDA LPAREN ID* RPAREN expr RPAREN) ]
+;;;         ;;; [(LPAREN expr expr RPAREN) ]
+
+;;;    )
+;;; )
+
+(define-syntax (simplify2 stx)
+    (syntax-case stx ()
+        ;;; [(ID) ID]
+        [(_ (expr _ inner_exp _)) #'(inner_exp)]
+        ;;; [(LPAREN PLUS expr expr RPAREN) (PLUS expr expr)]
+        ;;; [(LPAREN MINUS expr expr RPAREN) (MINUS expr expr)]
+        ;;; [(LPAREN TIMES expr expr RPAREN) (TIMES expr expr)]
+        ;;; [(LPAREN NOT expr RPAREN) (NOT expr)]
+        ;;; [(LPAREN MINUS expr RPAREN) (MINUS expr)]
+        ;;; [(LPAREN expr expr RPAREN) (APP expr expr)]
+        ;;; [(LPAREN LAMBDA LPAREN ID* RPAREN expr RPAREN) ]
+        ;;; [(LPAREN expr expr RPAREN) ]
+
+    )
+)
+
+
+;;; (simplify1 (syntax->datum (parse (tokenize (open-input-string "(+ 3 2)")))))
+(syntax->datum (simplify1 (parse (tokenize (open-input-string "(+ 3 2)")))))
+ 
