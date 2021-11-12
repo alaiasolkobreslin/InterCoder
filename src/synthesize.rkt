dbc8dcddd6f8abd5e3b6fd3d6302961eaa07fbe9
Alaia Solko-Breslin <alaiasolko-breslin@dhcp-vl2042-8049.redrover.cornell.edu>
2021-11-12 13:25:07 -0500

    synthesized plus

--- src/synthesize.rkt
+++ src/synthesize.rkt
@@ -3,7 +3,7 @@
 (require rosette)
 (require rosette/lib/synthax)     ; Require the sketching library.
 
-(define int32? (bitvector 32))
+; (define int32? (bitvector 32))
 
 (define-grammar (plus x y)        ; Grammar of int expressions over two inputs:
   [expr
@@ -11,19 +11,19 @@
            ((bop) (expr) (expr))  ;           (<bop> <expr> <expr>) |
            ((uop) (expr)))]       ;           (<uop> <expr>)
   [bop
-   (choose (+)  (-)  (*))]        ; <bop>  := plus  | minus | times |
+   (choose +  -  *)]        ; <bop>  := plus  | minus | times |
   [uop
-   (choose (not))])               ; <uop>  := not
+   (choose -)])                 ; <uop>  := neg
 
 (define (plus-interp n1 n2)
-  (plus n1 n2 #:depth 3))
+  (plus n1 n2 #:depth 2))
 
 (define (check-plus impl x y)
   (if (and (equal? x 1) (equal? y 2)) 
     (assert (equal? (impl x y) 3))
-    ; (if (and (equal? x 4) (equal? y 1))
-    ;   (assert (equal? (impl x y) 5))
-      (assert (equal? 1 1))))
+    (if (and (equal? x 4) (equal? y 1))
+      (assert (equal? (impl x y) 5))
+      (assert (equal? 1 1)))))
 
 (define-symbolic l h integer?)
 
