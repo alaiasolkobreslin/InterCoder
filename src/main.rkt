8fbe40576ad3dfde2254e9d9d032a3bb73b9c5c9
Alaia Solko-Breslin <ajs644@cornell.edu>
2021-11-23 14:14:18 -0500

    interpreter works without depth for now

--- src/main.rkt
+++ src/main.rkt
@@ -10,10 +10,15 @@
     (define file (read-line (current-input-port) 'any))
     (define contents (call-with-input-file file read-json))
     (define keys (hash-keys contents))
+    (define modu (hash-ref contents 'modulo))
+    (define examples (hash-ref modu 'examples))
     
     ; Will eventually delete this code below
     (printf "input: ~a, length: ~a, last character: ~a\n"
-        keys
+        examples
+        ; (define modu (hash-ref contents 'modulo))
+        ; (define example (hash-ref modu 'examples))
+        ; (list? examples)
         (string-length file)
         (char->integer (string-ref file (- (string-length file) 1))))
     (main))
