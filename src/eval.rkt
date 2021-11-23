23faae23ecb72f024ec871b0cfe208fd7f686b92
Alaia Solko-Breslin <ajs644@cornell.edu>
2021-11-23 14:14:45 -0500

    interpreter works without depth for now

--- src/eval.rkt
+++ src/eval.rkt
@@ -8,6 +8,7 @@
 (struct plus (e1 e2))
 (struct minus (e1 e2))
 (struct times (e1 e2))
+(struct modu (e1 e2))
 (struct neg (e))
 
 (define-grammar (binexpr x y)     ; Grammar of int expressions over two inputs:
@@ -27,20 +28,20 @@
   [uop
    (choose - !)])                 ; <uop>  := neg
 
-(define (plus-interp depth n1 n2)
-  (binexpr n1 n2 #:depth depth))
+(define (plus-interp n1 n2)
+  (binexpr n1 n2 #:depth 1))
 
-(define (minus-interp depth n1 n2)
-  (binexpr n1 n2 #:depth depth))
+(define (minus-interp n1 n2)
+  (binexpr n1 n2 #:depth 1))
 
-(define (times-interp depth n1 n2)
-  (binexpr n1 n2 #:depth depth))
+(define (times-interp n1 n2)
+  (binexpr n1 n2 #:depth 1))
 
-(define (modulo-interp depth n1 n2)
-  (binexpr n1 n2 #:depth depth))
+(define (modulo-interp n1 n2)
+  (binexpr n1 n2 #:depth 1))
 
-(define (neg-interp depth n1)
-  (unaryexpr n1 #:depth depth))
+(define (neg-interp n1)
+  (unaryexpr n1 #:depth 1))
 
 (define (assert-bin-examples js impl x y)
   (define examples (hash-ref js 'examples))
@@ -51,7 +52,7 @@
     (define y-hash (hash-ref ex 'y)) 
     (define res (hash-ref ex 'res))
     (if (and (equal? x x-hash) (equal? y y-hash)) 
-      (assert (equal? (impl depth x y) res))
+      (assert (equal? (impl x y) res))
       (assert (equal? 1 1))))) examples))
 
 (define (assert-unary-examples js impl x)
@@ -62,7 +63,7 @@
     (define x-hash (hash-ref ex 'x))
     (define res (hash-ref ex 'res))
     (if (equal? x x-hash) 
-      (assert (equal? (impl depth x) res))
+      (assert (equal? (impl x) res))
       (assert (equal? 1 1))))) examples))
 
 (define (check-plus js impl x y)
@@ -95,19 +96,24 @@
 (define sol
     (synthesize
      #:forall    (list l h)
-     #:guarantee (and (check-plus contents plus-interp l h) 
-     (check-minus contents minus-interp l h) 
-     (check-times contents times-interp l h)
-     (check-neg contents neg-interp l)
-     (check-modulo contents modulo-interp l h))))
+     #:guarantee (and 
+      (check-plus contents plus-interp l h) 
+      (check-minus contents minus-interp l h) 
+      (check-times contents times-interp l h)
+      (check-neg contents neg-interp l)
+      (check-modulo contents modulo-interp l h))))
 
 sol
 (print-forms sol)
 
-; (define (eval e)
-;   (match e
-;     [(num i) i]
-;     [(plus e1 e2) (evaluate (plus-interp (eval e1) (eval e2)) sol)]
-;     [(minus e1 e2) (evaluate (minus-interp (eval e1) (eval e2)) sol)]))
+(define (eval e)
+  (match e
+    [(num i) i]
+    [(plus e1 e2) (evaluate (plus-interp (eval e1) (eval e2)) sol)]
+    [(minus e1 e2) (evaluate (minus-interp (eval e1) (eval e2)) sol)]
+    [(times e1 e2) (evaluate (times-interp (eval e1) (eval e2)) sol)]
+    [(modu e1 e2) (evaluate (modulo-interp (eval e1) (eval e2)) sol)]
+    [(neg e1) (evaluate (neg-interp (eval e1)) sol)]))
 
-; (eval (plus (minus (num 10)(num 2)) (num 2)))
+(define expr (plus (minus (num 10)(num 2)) (num 2)))
+(eval expr)
